89148ad01790b6bb97f716ed2ae17b4db836e69a
Alex Tsang <alextsang@live.com>
2021-04-24 18:35:52 +0900

    Type definitions

---
+++ src/types/app-config.d.ts
@@ -0,0 +1,17 @@
+export type AppConfig = {
+
+  /**
+   * GraphicsMagick executable path.
+   */
+  gmPath: string;
+
+  /**
+   * Maximum width of a resized image.
+   */
+  maxWidth: number;
+
+  /**Maximum height of a resized image.
+   *
+   */
+  maxHeight: number;
+}
