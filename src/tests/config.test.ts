8dbb0a5dc81bacabb78ca7a957b145fd95f31a85
Alex Tsang <alextsang@live.com>
2021-04-25 20:07:23 +0900

    Configurations module test cases

---
+++ src/tests/config.test.ts
@@ -0,0 +1,15 @@
+import assert from 'assert';
+
+import config from '../config';
+
+const GM_PATH: string = 'C:\\Program Files\\GraphicsMagick-1.3.36-Q8\\gm.exe'
+const MAX_WIDTH: number = 1280;
+const MAX_HEIGHT: number = 720;
+
+describe('Configurations module', async (): Promise<void> => {
+  it('can load configurations', async (): Promise<void> => {
+    assert.strictEqual(config.gmPath, GM_PATH);
+    assert.strictEqual(config.maxWidth, MAX_WIDTH);
+    assert.strictEqual(config.maxHeight, MAX_HEIGHT);
+  });
+});
