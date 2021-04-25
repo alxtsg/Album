366606169e1882ecf1c48fea10721e87a4e78e0d
Alex Tsang <alextsang@live.com>
2021-04-25 23:37:43 +0900

    Prepare configurations for testing on both Windows and UNIX environment

--- src/tests/config.test.ts
+++ src/tests/config.test.ts
@@ -3,8 +3,8 @@ import assert from 'assert';
 import config from '../config';
 
 const GM_PATH: string = 'C:\\Program Files\\GraphicsMagick-1.3.36-Q8\\gm.exe'
-const MAX_WIDTH: number = 1280;
-const MAX_HEIGHT: number = 720;
+const MAX_WIDTH: number = 100;
+const MAX_HEIGHT: number = 100;
 
 describe('Configurations module', async (): Promise<void> => {
   it('can load configurations', async (): Promise<void> => {
--- src/tests/setup.ts
+++ src/tests/setup.ts
@@ -1,13 +1,30 @@
 import fs from 'fs';
+import os from 'os';
 import path from 'path';
 
-const ENV_TEMPLATE: string = path.join(__dirname, '..', '.env.template');
+const WINDOWS_PLATFORM: string = 'win32';
 const ENV_FILE: string = path.join(__dirname, '..', '.env');
+const UNIX_ENV_CONTENT: string = [
+  'GM_PATH=gm',
+  'MAX_WIDTH=100',
+  'MAX_HEIGHT=100'
+].join('\n');
+const WINDOWS_ENV_CONTENT: string = [
+  'GM_PATH=C:\\Program Files\\GraphicsMagick-1.3.36-Q8\\gm.exe',
+  'MAX_WIDTH=100',
+  'MAX_HEIGHT=100'
+].join('\n');
 
 const fsPromises = fs.promises;
 
 const main = async (): Promise<void> => {
-  await fsPromises.copyFile(ENV_TEMPLATE, ENV_FILE);
+  let content: string | null = null;
+  if (os.platform() === WINDOWS_PLATFORM) {
+    content = WINDOWS_ENV_CONTENT;
+  } else {
+    content = UNIX_ENV_CONTENT;
+  }
+  await fsPromises.writeFile(ENV_FILE, content)
 };
 
 main();
