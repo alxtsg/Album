2e46d3322e68440c562bc6c7266c8b4cbd992638
Alex Tsang <alextsang@live.com>
2024-03-14 04:26:19 +0900

    Improve config loader test cases

--- src/tests/config.test.ts
+++ src/tests/config.test.ts
@@ -3,12 +3,14 @@ import assert from 'assert';
 import config from '../config';
 
 const THUMBNAIL_FORMAT = 'jpeg';
+const VIDEO_FORMAT = 'mp4';
 const MAX_WIDTH= 100;
 const MAX_HEIGHT = 100;
 
 describe('Configurations module', async () => {
   it('can load configurations', async () => {
     assert.strictEqual(config.thumbnailFormat, THUMBNAIL_FORMAT);
+    assert.strictEqual(config.videoFormat, VIDEO_FORMAT);
     assert.strictEqual(config.maxWidth, MAX_WIDTH);
     assert.strictEqual(config.maxHeight, MAX_HEIGHT);
   });
