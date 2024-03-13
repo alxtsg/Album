9d9e0fe8966a855506124d9eb697ea70ed8d0b67
Alex Tsang <alextsang@live.com>
2024-03-14 01:38:09 +0900

    Refactor page utils to accept more than photo objects

--- src/page-utils.ts
+++ src/page-utils.ts
@@ -3,19 +3,19 @@ import mustache from 'mustache';
 import fsPromises from 'fs/promises';
 import path from 'path';
 
-import type Photo from './types/photo';
+import type MediaView from './types/media-view';
 
 const TEMPLATE: string = path.join(__dirname, 'template.mustache');
 
 /**
- * Generates a page using the given photos.
+ * Generates a page using the given media file views.
  *
- * @param photos Photos to be embedded in the page.
- * @param page Absolute path of the generated page.
+ * @param mediaViews Media file view objects.
+ * @param pagePath Absolute path of the generated page.
  *
  * @returns A Promise resolves without a value.
  */
-export const generatePage = async (photos: Photo[], page: string) => {
+export const generatePage = async (mediaViews: MediaView[], pagePath: string) => {
   const templateContent = await fsPromises.readFile(
     TEMPLATE,
     {
@@ -24,8 +24,8 @@ export const generatePage = async (photos: Photo[], page: string) => {
   );
   const view = {
     currentTimestamp: (new Date()).toISOString(),
-    photos
+    mediaFiles: mediaViews
   };
   const pageContent = mustache.render(templateContent, view);
-  await fsPromises.writeFile(page, pageContent);
+  await fsPromises.writeFile(pagePath, pageContent);
 };
